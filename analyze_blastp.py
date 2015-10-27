b97e676bda6049d565f5d57f418f04233112ad63
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2015-10-26 22:24:44 -0400

    ENH: Use new filtered blastp output

--- analyze_blastp.py
+++ analyze_blastp.py
@@ -6,7 +6,7 @@ import os
 # Disable table wrapping
 pd.set_option('expand_frame_repr', False)
 
-BLASTP_EXT = '.faa.blastp'
+BLASTP_OUTPUT_FILE = '../data/blastp-output/all.fasta.blastp'
 
 def blastp_output(blastp_file):
     '''Read output from NIH BLAST+ `blastp` command-line program run with
@@ -29,27 +29,12 @@ def blastp_output(blastp_file):
 
 if __name__ == '__main__':
     # Read each file, adding a homologous group column
-    df = pd.DataFrame()
-    dirname = '../data/blastp-output/'
-    for blastp_file in os.listdir(dirname):
-        df_homologs = blastp_output(os.path.join(dirname, blastp_file))
-        # Strip file extension
-        homologs = blastp_file[:-len(BLASTP_EXT)]
-        df_homologs['query homologs'] = homologs
-        # Append to single DataFrame
-        df = pd.concat([df, df_homologs])
-    # Re-enumerate the index
-    df = df.reset_index()
+    df = blastp_output(BLASTP_OUTPUT_FILE)
     # Get the best hit of each query
     q_grouped = df.groupby('query id')
     max_idx = q_grouped['bit score'].idxmax().values
     max_idx.sort()
     df_max = df.iloc[max_idx]
-
-    # Which homologous groups produce so many results?
-    h_grouped = df_max.groupby(['query homologs', 'query id'])
-    assert h_grouped.count()['subject id'].max() == 1
-    df_max[['query homologs', 'subject id']]
 
     # Write the subject ids to disk for samtools faidx
     arr_subj = df_max['subject id'].unique()
