ba9252cabb5609f7cf0d0d1c1c9f9ec577f043d8
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2015-10-26 21:53:49 -0400

    ENH: Use blastp built-in threading instead of GNU parallel

--- create-blastp.sh
+++ create-blastp.sh
@@ -2,21 +2,18 @@
 # Create blastp output files using NIH BLAST+ `blastp` command-line
 # program.
 
-BLAST_INPUT_DIR="/Volumes/Macintosh HD 2/thiberio/t3ss_fasta_files/"
-BLAST_DB="../blast_dbs/nr"
-BLAST_NEGATIVE_GILIST="~/data/blastp-input/sequence.gi.txt"
-PARALLEL_LOG_DIR="~/code/log"
+BLAST_DIR="/Volumes/Macintosh HD 2/thiberio/blast_dbs/"
+BLAST_DB="nr"
+BLAST_INPUT_FILE="/Users/omsai/data/blastp-input/all.fasta"
+BLAST_OUTPUT_FILE="/Users/omsai/data/blastp-output/$(basename ${BLAST_INPUT_FILE}).blastp"
+BLAST_NEGATIVE_GILIST="/Users/omsai/data/blastp-input/sequence.gi"
 
-( cd "${BLAST_INPUT_DIR}"
-  find . -iname '*.faa' |
-      LC_ALL=C \
-	    parallel \
-	    --jobs 12 \
-	    blastp \
-	    -query "{}" \
-	    -db "${BLAST_DB}" \
-	    -outfmt 7 \
-	    -negative_gilist "${BLAST_NEGATIVE_GILIST}" \
-	    "> ~/data/blastp-output/{/}.blastp"
-  cd - > /dev/null		# Return to original directory.
+# Use a subshell (enclosing the commands in parantheses) so that
+# failure of any of these commands does not change the working
+# directory.
+(
+    cd "${BLAST_DIR}"
+    pwd
+    blastp -num_threads 12 -db ${BLAST_DB} -query ${BLAST_INPUT_FILE} -negative_gilist ${BLAST_NEGATIVE_GILIST} -out ${BLAST_OUTPUT_FILE} -outfmt 7\ qseqid\ sseqid\ qlen\ pident\ length\ mismatch\ gapopen\ qstart\ qend\ sstart\ send\ evalue\ bitscore
+    cd - > /dev/null		# Return to original directory.
 )
