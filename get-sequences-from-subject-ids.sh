e7ecd1e3fba435fb35f724a20557bcedd72552c0
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2015-10-26 23:30:03 -0400

    BUG: Complete samtools parsing

--- get-sequences-from-subject-ids.sh
+++ get-sequences-from-subject-ids.sh
@@ -1,2 +1,9 @@
-cd '/Volumes/Macintosh HD 2/thiberio/blast_dbs'
-cat ~/data/analyze-output/subject_ids.ref | time samtools faidx nr
+refs_file=/Users/omsai/data/analyze-output/subject_ids.ref
+output_file=/Users/omsai/data/analyze-output/samtools-faidx.faa
+
+ids_inline=$(cat ${refs_file} | tr '\n' ' ')
+(
+    cd '/Volumes/Macintosh HD 2/thiberio/blast_dbs'
+    time samtools faidx nr ${ids_inline} | tee ${output_file}
+    cd - > /dev/null
+)
