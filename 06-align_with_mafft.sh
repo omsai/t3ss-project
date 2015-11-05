0cb8ecbdf6bc088ee1974864e34d700ac1841c3e
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2015-11-04 19:24:08 -0500

    ENH: New sequence alignment script

---
+++ 06-align_with_mafft.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# mafft does not perform well multi threaded, so we will use it
+# serially and parallelize it using GNU Parallel.
+
+in_dir='../data/mafft-input'
+out_dir='../data/mafft-output'
+log_dir='log'
+
+ls ${in_dir}/*.faa | \
+    parallel \
+	--jobs 50% \
+	--joblog ${log_dir}/parallel-mafft.log \
+	--resume-failed \
+	mafft "> ${out_dir}/{/.}.mafft"
