72eb0dcbb077cb1ba0363360d6a4c85a9e812faf
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2015-11-19 21:52:44 -0500

    ENH: Annotate node names and export trees
    - Annotate node names with taxa.  Read the taxa from teh MAFFT
      alignment sequence comments.  Reading such information from the
      comments should really be a separate function; maybe in a separate
      library as reading the comments was also done in the NR reader.
    
    - Export the annotated trees into separate directories for
      paraphyletic and monophyletic.
    
    - Fix nomenclature:
      - Monophylic -> Monophyletic
      - Polyphylic -> Paraphyletic
    
    - Fix printing to reflect that paraphyletic trees are of interest for
      HGT, not monophyletic.

--- 08-monophyly.py
+++ 08-monophyly.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-'''Test trees for monophily.'''
-DEBUG = False
-TREE_FORMAT='newick'
+'''Test trees for polyphily and reexport with taxa information.'''
 
+from __future__ import print_function
 import Bio.Phylo as phy
 from blastplus import BlastProcessor, BLASTP_OUTPUT_FILE
 import os
+import re                       # For extracting taxa
+
 TREE_DIR = '../data/fasttree-output/'
+TREE_FORMAT = 'newick'
+SEQ_DIR = '../data/mafft-output/'
+SEQ_EXT = 'mafft'
+OUTPUT_DIR = '../data/phylo-output/'
 
 # Read all the files in as trees
 filenames = os.listdir(TREE_DIR)
@@ -19,17 +24,75 @@ queries = bp.df['query id'].unique()
 
 # Test for monophyly
 digits = len(str(len(trees)))
-is_monophylic = []
-for tree in trees:
+is_monophyletic = []
+for tree, filename in zip(trees, files):
     all_terminals = tree.get_terminals()
+
+    # Ignore <= 3 taxa trees.
+    if len(all_terminals) <= 3:
+        is_monophyletic += ['Ignored due to <= 3 terminals']
+        continue
+
+    # Read taxa names of gi blast results
+    tree_filename = os.path.basename(filename)
+    seq_filename = os.path.splitext(tree_filename)[0] + '.' + SEQ_EXT
+    seq_file = os.path.join(SEQ_DIR, seq_filename)
+    with open(seq_file, 'rU') as fp:
+        lines = fp.readlines()
+        comments = [line[1:-1] for line in lines if line[0] == '>']
+    gene_to_taxa = {}
+    regex_gene = r'(gi\|[0-9]+\|[a-z]+\|[^\|]+\|)'
+    regex_sep = r'[^\[]+\['
+    regex_taxa = r'([^\]]+)'
+    for comment in comments:
+        if comment.count(' ') > 0:
+            pairs = re.findall(regex_gene + regex_sep + regex_taxa, comment)
+            genes = [] # DEBUG
+            for pair in pairs:
+                gene, taxa = pair
+                gene_to_taxa[gene] = taxa
+                genes += [gene] # DEBUG
+            assert len(pairs) == len([gene_to_taxa[gene] for gene in genes])
+
     aeromonas_terminals = []
-    for terminal in all_terminals:
+    for i in xrange(len(all_terminals)):
+        terminal = all_terminals[i]
         if terminal.name in queries:
             aeromonas_terminals += [terminal]
-    if DEBUG:
-        print("{0:>{digits}}/{1:>{digits}} terminals are Aeromonas".format(
-            len(aeromonas_terminals), len(all_terminals), digits=digits))
-    is_monophylic += [tree.is_monophyletic(aeromonas_terminals)]
+        else:
+            # Append taxa names to gi blast results
+            gene = re.findall(regex_gene, all_terminals[i].name)[0]
+            if gene_to_taxa.has_key(gene):
+                all_terminals[i].name += gene_to_taxa[gene].replace(' ', '_')
+            else:
+                print('Warning: unable to find taxa name of gene {}'.format(gene))
 
-num_monophylic = len(is_monophylic) - is_monophylic.count(False)
-print('{0} of {1} trees are monophylic'.format(num_monophylic, len(is_monophylic)))
+    tree.root_at_midpoint()
+    is_monophyletic += [tree.is_monophyletic(aeromonas_terminals)]
+
+    # Print paraphyletic trees
+    if is_monophyletic[-1] == False:
+        num_aero = len(aeromonas_terminals)
+        num_all = len(all_terminals)
+        print("{diff:>{digits}} blast hits, {aero:>{digits}} Aeromonas, ".format(
+            aero=num_aero, digits=digits, diff=num_all-num_aero), end='')
+        print(tree_filename)
+
+# Save all the paraphyletic trees
+output_files = []
+num_paraphyletic = 0
+for i, tree in enumerate(trees):
+    if is_monophyletic[i]:
+        sub_dir = 'monophyletic'
+    else:
+        sub_dir = 'paraphyletic'
+        num_paraphyletic += 1
+    filename = filenames[i]
+    output_filename = os.path.splitext(filename)[0] + '.' + TREE_FORMAT
+    output_file = os.path.join(OUTPUT_DIR, sub_dir, filename)
+    output_files += [output_file]
+
+map(phy.write, trees, output_files,
+    [TREE_FORMAT] * len(trees))
+print('Finished writing {} trees'.format(len(output_files)))
+print('{} of {} trees are paraphyletic'.format(num_paraphyletic, len(trees)))
